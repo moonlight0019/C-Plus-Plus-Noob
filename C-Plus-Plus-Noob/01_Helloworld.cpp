840fadb2e690f877251dcb5ef2da792d7b17b366
moonlight0019 <moonlight19c@gmail.com>
2024-05-23 19:04:09 +0800

    Add 01_Helloworld.cpp

--- C-Plus-Plus-Noob/01_Helloworld.cpp
+++ C-Plus-Plus-Noob/01_Helloworld.cpp
@@ -2,10 +2,16 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
+    cout << "Hello World!\n";
+
+    system("pause");
+
+    return 0;
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
